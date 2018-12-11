45535807c08d41ae53ddd6918c0254a19527489b
Samyr <samyr.anrb@gmail.com>
2018-12-10 23:40:51 -0200

    showing user accounts on django admin

--- medium/accounts/admin.py
+++ medium/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+
+class accountsAdmin(admin.ModelAdmin): #classe para customizar o admin
+    list_display = ['name','username','email','date_joined'] #campos listados no admin
+    search_fields = ['name', 'email'] #exibe campo de pesquida no admin
+    #prepopulated_fields = {'slug': ('name',)}
+
+
+
+admin.site.register(User)
