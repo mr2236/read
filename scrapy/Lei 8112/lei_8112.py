9b5bc56c64ccfa72d01c0f69df470986670d5e9f
samyr <samyr.anrb@gmail.com>
2018-12-14 08:16:43 -0200

    scraping lei 8666, 9784, 8429

---
+++ scrapy/Lei 8112/lei_8112.py
@@ -0,0 +1,41 @@
+#Scrapy feito em 12/12
+
+import requests, sqlite3
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
+from datetime import date, datetime
+
+conn = sqlite3.connect('../../db.sqlite3', check_same_thread=False)
+#conn = psycopg2.connect('postgres://gumpusaddqrkcg:3894ca3d0276a4ed486e2435681c8da47bd6eae270322a5a4b532ad320f4cc80@ec2-54-235-156-60.compute-1.amazonaws.com:5432/d2p9n2h48bq3sj')
+
+with open('lei_8112.html', 'rb') as f:
+#with open('/app/scrapy/cfederal.html', 'rb') as f:
+    soup = BeautifulSoup(f, 'html.parser')
+for tag in soup.findAll():
+    if tag.name in ['a', 'strike']:
+      tag.decompose()
+
+i = 0
+is_titulo = False
+allp = soup.findAll('p')
+for p in allp:
+    nonBreakSpace = u'\xa0'
+    result = p.text#.replace('\n', '').replace(nonBreakSpace, '')  
+    curs = conn.cursor()
+    if len(result) > 5:     
+        # if p.span != None:   
+        #     if p.span["style"]=="text-transform: uppercase":       
+        #         is_titulo = True            
+        if p.get('align') == 'CENTER':
+             is_titulo = True               
+        else:
+             result = p.text.replace('\n', ' ')#.replace(nonBreakSpace, '')  
+        print(result)
+        curs.execute("INSERT INTO leis_artigo ( artigo, created_at, updated_at, lei_id, id_artigo_lei, is_titulo) values (?, ?, ?, ?, ?, ?)", (result, date.today(), date.today(),  '2' , i, is_titulo))
+        #curs.execute("INSERT INTO leis_artigo ( artigo, created_at, updated_at, lei_id, id_artigo_lei, is_titulo) values (%s, %s, %s, %s, %s, %s)", (result, date.today(), date.today(),  '1' , i, is_titulo))
+
+        is_titulo = False
+        i = i+1 
+conn.commit()
+
+
