05b20a16abf7f9d2fcac6d5c1b556a68543f8e11
Samyr <samyr.anrb@gmail.com>
2018-12-14 18:55:57 -0200

    scraping

--- scrapy/ConstituicaoFederal/constituicao_federal.py
+++ scrapy/ConstituicaoFederal/constituicao_federal.py
@@ -9,7 +9,7 @@ from datetime import date, datetime
 conn = psycopg2.connect('postgres://gumpusaddqrkcg:3894ca3d0276a4ed486e2435681c8da47bd6eae270322a5a4b532ad320f4cc80@ec2-54-235-156-60.compute-1.amazonaws.com:5432/d2p9n2h48bq3sj')
 
 #with open('cfederal.html', 'rb') as f:
-with open('/app/scrapy/Constituição Federal/cfederal.html', 'rb') as f:    
+with open('/app/scrapy/ConstituicaoFederal/cfederal.html', 'rb') as f:    
     soup = BeautifulSoup(f, 'html.parser')
 for tag in soup.findAll():
     if tag.name in ['a', 'strike']:
@@ -29,7 +29,7 @@ for p in allp:
         elif p.get('align') == 'CENTER':
              is_titulo = True               
         else:
-             result = p.text.replace('\n', '').replace(nonBreakSpace, '')  
+             result = p.text.replace('\n', ' ').replace(nonBreakSpace, '')  
 
         #curs.execute("INSERT INTO leis_artigo ( artigo, created_at, updated_at, lei_id, id_artigo_lei, is_titulo) values (?, ?, ?, ?, ?, ?)", (result, date.today(), date.today(),  '1' , i, is_titulo))
         curs.execute("INSERT INTO leis_artigo ( artigo, created_at, updated_at, lei_id, id_artigo_lei, is_titulo) values (%s, %s, %s, %s, %s, %s)", (result, date.today(), date.today(),  '1' , i, is_titulo))
